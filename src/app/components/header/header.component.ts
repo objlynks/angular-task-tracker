e9117f281a1ed81c9d3bfdac929a1ec8c3edfd93
Obed Jonathan <obedjonathan26@gmail.com>
2022-04-20 17:24:01 +0100

    Task tracker setup

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title : string = "Task Tracker";
+  btnColor : any;
+  btnText : any;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.btnColor = "blue";
+    this.btnText = "Add";
+  }
+
+}
